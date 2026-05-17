40ddba0499ae042c96cfe6bd8c916a6465ad996d
Aman Verma <amanverma10.connect@gmail.com>
2026-05-17 23:18:25 +0530

    feat(layout): add global header and footer to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,13 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    > 
+
+      <body className="min-h-full flex flex-col">
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+      </body>
     </html>
   );
 }
